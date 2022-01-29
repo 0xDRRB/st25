d1949fda26005247b20e8290e4b540dac9052482
Denis Bodor <lefinnois@lefinnois.net>
2022-01-29 17:55:34 +0100

    send APDU by string + color output

--- main.c
+++ main.c
@@ -10,6 +10,10 @@
 
 #include <nfc/nfc.h>
 
+#include "color.h"
+
+#define RESPMAXSZ 32
+
 nfc_device *pnd;
 nfc_context *context;
 
@@ -26,6 +30,9 @@ struct st25taCC_t {
 	uint8_t writeaccess;
 };
 
+// https://en.wikipedia.org/wiki/Smart_card_application_protocol_data_unit
+// https://www.st.com/resource/en/datasheet/st25ta64k.pdf
+// https://www.eftlab.com/knowledge-base/complete-list-of-apdu-responses/
 
 int param_gethex_to_eol(const char *line, uint8_t *data, int maxdatalen, int *datalen) {
     uint32_t temp;
@@ -83,47 +90,65 @@ static void sighandler(int sig)
     exit(EXIT_FAILURE);
 }
 
-int CardTransmit(nfc_device *pnd, uint8_t *capdu, size_t capdulen, uint8_t *rapdu, size_t *rapdulen, int debug)
-{
-    int res;
-    size_t  szPos;
-
-	if(debug) {
-		printf("=> ");
-		for (szPos = 0; szPos < capdulen; szPos++) {
-			printf("%02x ", capdu[szPos]);
-		}
-		printf("\n");
-	}
-
-    if((res = nfc_initiator_transceive_bytes(pnd, capdu, capdulen, rapdu, *rapdulen, 500)) < 0) {
-        fprintf(stderr, "nfc_initiator_transceive_bytes error! %s\n", nfc_strerror(pnd));
-        return -1;
-    }
-
-	if(debug) {
-		printf("<= ");
-		for (szPos = 0; szPos < res; szPos++) {
-			printf("%02x ", rapdu[szPos]);
-		}
-		printf("\n");
-	}
-
-	*rapdulen = (size_t)res;
-	return 0;
-}
-
-int strCardTransmit(nfc_device *pnd, uint8_t *capdu, size_t capdulen, uint8_t *rapdu, size_t *rapdulen, int debug)
+int strCardTransmit(nfc_device *pnd, const char *line, uint8_t *rapdu, size_t *rapdulen, int debug)
 {
     int res;
     size_t szPos;
+	uint8_t *capdu = NULL;
+	size_t capdulen = 0;
+
+	uint32_t temp;
+	int indx = 0;
+	char buf[5] = {0};
+
+	// linelen >0 & even
+	if(!strnlen(line, 64) || strnlen(line, 64) % 2)
+		return -1;
+
+	capdu = malloc(strnlen(line, 64) / 2);
+	if(!capdu) {
+		fprintf(stderr, "malloc list error: %s\n", strerror(errno));
+		nfc_close(pnd);
+		nfc_exit(context);
+		exit(EXIT_FAILURE);
+	}
+
+    while (line[indx]) {
+        if (line[indx] == '\t' || line[indx] == ' ') {
+            indx++;
+            continue;
+        }
+
+        if (isxdigit(line[indx])) {
+            buf[strlen(buf) + 1] = 0x00;
+            buf[strlen(buf)] = line[indx];
+        } else {
+            // if we have symbols other than spaces and hex
+			free(capdu);
+            return -1;
+        }
+
+        if (strlen(buf) >= 2) {
+            sscanf(buf, "%x", &temp);
+            capdu[capdulen] = (uint8_t)(temp & 0xff);
+            *buf = 0;
+            capdulen++;
+        }
+        indx++;
+    }
+
+    //error when not completed hex bytes
+    if(strlen(buf) > 0) {
+		free(capdu);
+		return -1;
+	}
 
 	if(debug) {
-		printf("=> ");
+		printf(YELLOW "=> " );
 		for (szPos = 0; szPos < capdulen; szPos++) {
 			printf("%02x ", capdu[szPos]);
 		}
-		printf("\n");
+		printf(RESET "\n");
 	}
 
     if((res = nfc_initiator_transceive_bytes(pnd, capdu, capdulen, rapdu, *rapdulen, 500)) < 0) {
@@ -132,17 +157,18 @@ int strCardTransmit(nfc_device *pnd, uint8_t *capdu, size_t capdulen, uint8_t *r
     }
 
 	if(debug) {
-		printf("<= ");
+		printf(GREEN "<= ");
 		for (szPos = 0; szPos < res; szPos++) {
 			printf("%02x ", rapdu[szPos]);
 		}
-		printf("\n");
+		printf(RESET "\n");
 	}
 
 	*rapdulen = (size_t)res;
+
+	free(capdu);
 	return 0;
 }
-
 
 static void print_hex(const uint8_t *pbtData, const size_t szBytes)
 {
@@ -156,6 +182,9 @@ static void print_hex(const uint8_t *pbtData, const size_t szBytes)
 
 int main(int argc, const char *argv[])
 {
+	uint8_t resp[RESPMAXSZ] = {0};
+	size_t respsz = RESPMAXSZ;
+
 	nfc_target nt;
 
 	// Initialize libnfc and set the nfc_context
@@ -215,32 +244,18 @@ int main(int argc, const char *argv[])
 		}
 	}
 
-	//     Cl  ins p1  p2  lc                              Le
-	// 02  00  a4  04  00  07  d2  76  00  00  85  01  01  00
-
-	// https://en.wikipedia.org/wiki/Smart_card_application_protocol_data_unit
-	//                          class   ins   P1    P2    Lc    1     2     3     4     5     6     6     Le
-	uint8_t apdu_selectapp[] = { 0x00, 0xa4, 0x04, 0x00, 0x07, 0xd2, 0x76, 0x00, 0x00, 0x85, 0x01, 0x01, 0x00 };
-	size_t apdusz = 13;
-	uint8_t resp[32] = {0};
-	size_t respsz = 32;
-
-	if(CardTransmit(pnd, apdu_selectapp, apdusz, resp, &respsz, 1) < 0)
+	respsz = RESPMAXSZ;
+	//                  class   ins   P1    P2    Lc    1     2     3     4     5     6     6     Le
+	if(strCardTransmit(pnd, "00 a4 04 00 07 d2 76 00 00 85 01 01 00", resp, &respsz, 1) < 0)
 		fprintf(stderr, "CardTransmit error!\n");
 
-	// https://www.eftlab.com/knowledge-base/complete-list-of-apdu-responses/
 	if(respsz < 2 || resp[respsz-2] != 0x90 || resp[respsz-1] != 0x00)
 		fprintf(stderr, "Bad response !\n");
 	else
 		printf("Success! App selected.\n");
 
-	// https://www.st.com/resource/en/datasheet/st25ta64k.pdf
-	//                              class  ins   P1    P2    Lc    1     2    ( Le --> 67 00 --> Wrong length)
-	uint8_t apdu_selectfilecc[] = { 0x00, 0xa4, 0x00, 0x0c, 0x02, 0xe1, 0x03 };
-	apdusz = 7;
-	respsz = 32;
-
-	if(CardTransmit(pnd, apdu_selectfilecc, apdusz, resp, &respsz, 1) < 0)
+	respsz = RESPMAXSZ;
+	if(strCardTransmit(pnd, "00 a4 00 0c 02 e1 03", resp, &respsz, 1) < 0)
 		fprintf(stderr, "CardTransmit error!\n");
 
 	if(respsz < 2 || resp[respsz-2] != 0x90 || resp[respsz-1] != 0x00)
@@ -248,12 +263,8 @@ int main(int argc, const char *argv[])
 	else
 		printf("Success! Capability Container file selected.\n");
 
-	//                              class  ins   P1    P2    Le
-	uint8_t apdu_selectreadcc[] = {	0x00, 0xb0, 0x00, 0x00, 0x0f };
-	apdusz = 5;
-	respsz = 32;
-
-	if(CardTransmit(pnd, apdu_selectreadcc, apdusz, resp, &respsz, 1) < 0)
+	respsz = RESPMAXSZ;
+	if(strCardTransmit(pnd, "00 b0 00 00 0f", resp, &respsz, 1) < 0)
 		fprintf(stderr, "CardTransmit error!\n");
 
 	if(respsz < 2 || resp[respsz-2] != 0x90 || resp[respsz-1] != 0x00)
